24755803aafae14587a4f192b2f526339c30b49f
Illia Lohynenko <illia.lohvynenko@sudent.tuke.sk>
2025-07-17 12:04:14 +0200

    Обновлён текст приветствия

--- src/App.js
+++ src/App.js
@@ -17,58 +17,53 @@ function App() {
     <div
       className="app"
       style={{
-        backgroundImage: 'url(/Logos.png)', // Картинка из public
-        backgroundSize: 'cover',
-        backgroundPosition: 'center',
-        backgroundAttachment: 'fixed',
-        backgroundRepeat: 'no-repeat',
+        backgroundImage: 'url(/Logos.png)',
       }}
     >
-      <div className="welcome">
-        <img
-          src="https://upload.wikimedia.org/wikipedia/en/9/98/Joker_%28The_Dark_Knight%29.jpg"
-          alt="Joker"
-          className="joker-img"
-        />
-        <p>
-          Ласкаво прошу до пекла, клоуне!<br />
-          Ахахаха!<br />
-          Одна помилка — і ви програли!<br />
-          Не засмучуйте мене,<br />
-          я тільки почав веселитися!
-        </p>
-      </div>
+      <div className="overlay">
+        <div className="welcome">
+          <img
+            src="https://upload.wikimedia.org/wikipedia/en/9/98/Joker_%28The_Dark_Knight%29.jpg"
+            alt="QUEST"
+            className="joker-img"
+          />
+<p>
+  Pridajte sa k nám na našu malú hru pre vás
+</p>
 
-      <div className="video-container">
-        <iframe
-          width="560"
-          height="315"
-          src="https://www.youtube.com/embed/EXeTwQWrcwY"
-          title="Joker"
-          frameBorder="0"
-          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        ></iframe>
-      </div>
-
-      <div className="password-section">
-        <label htmlFor="password">Введіть пароль: </label>
-        <input
-          type="text"
-          id="password"
-          value={input}
-          onChange={(e) => setInput(e.target.value)}
-        />
-        <button onClick={handleClick}>Відправити</button>
-      </div>
-
-      {message && (
-        <div className="message">
-          {message.split('\n').map((line, i) => (
-            <p key={i}>{line}</p>
-          ))}
         </div>
-      )}
+
+        <div className="video-container">
+          <iframe
+            width="560"
+            height="315"
+            src="https://www.youtube.com/embed/EXeTwQWrcwY"
+            title="Joker"
+            frameBorder="0"
+            allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          ></iframe>
+        </div>
+
+        <div className="password-section">
+          <label htmlFor="password">Введіть пароль: </label>
+          <input
+            type="text"
+            id="password"
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <button onClick={handleClick}>Відправити</button>
+        </div>
+
+        {message && (
+          <div className="message">
+            {message.split('\n').map((line, i) => (
+              <p key={i}>{line}</p>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
